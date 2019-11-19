333dd25a10ea0960aa12e5af5d029b1c5ce9f457
Leonardo Loyo <lloyo@mediastre.am>
2019-11-19 16:08:00 -0300

    add mediastream-ads

--- react-native.config.js
+++ react-native.config.js
@@ -10,11 +10,6 @@ module.exports = {
         android: null,
       },
     },
-    'react-native-mediastream-ads': {
-      platforms: {
-        ios: null, // disable ios platform, other platforms will still autolink if provided
-      },
-    },
     'react-native-google-cast': {
       platforms: {
         ios: null,
